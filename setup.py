472046e4af97e6368e818ae454ac1594da419bf8
Swaraj Jalkote <swarajjalkote98@gmail.com>
2022-11-06 00:11:11 +0530

    Added required files to make a library

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Developers',
+  'Operating System :: Microsoft :: Windows :: Windows 11',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='csv_to_dat',
+  version='0.0.1',
+  description='CSV to DAT convertor with additional options to make the conversion more efficient and flexibility to add or remove columns from the conversion',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='https://github.com/SwarajJalkote/csv_to_dat',  
+  author='Swaraj Jalkote',
+  author_email='swarajjalkote98@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords=['csv', 'dat', 'convertor'], 
+  packages=find_packages(),
+  install_requires=['setuptools', 'pandas'] 
+)
