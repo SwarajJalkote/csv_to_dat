4a3df750588d69c82c2cd4b2690e1ef61b953ab0
Swaraj Jalkote <swarajjalkote98@gmail.com>
2022-11-06 01:05:57 +0530

    Added demo csv file and updated the code

--- src/csv_to_dat.py
+++ src/csv_to_dat.py
@@ -1,11 +1,15 @@
 
 import pandas as pd
+import typing
 
 
-
-def read_and_convert(filename, *args):
-    pass
+class CsvToDat:
+    def __init__(self) -> None:
+        pass
+    def read_and_convert(filename, *args):
+        pass
 
 
 if __name__ == "__main__":
-    read_and_convert("filename.csv")
+    #read_and_convert("filename.csv")
+    pass
