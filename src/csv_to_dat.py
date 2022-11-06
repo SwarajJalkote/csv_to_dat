07d869bdee022f276899627f567cbfcf2e459b1f
Swaraj Jalkote <swarajjalkote98@gmail.com>
2022-11-06 17:47:50 +0530

    Added function to read the csv file into pandas DataFrame

--- src/csv_to_dat.py
+++ src/csv_to_dat.py
@@ -1,15 +1,38 @@
 
 import pandas as pd
 import typing
+import traceback
+import os
 
 
-class CsvToDat:
-    def __init__(self) -> None:
-        pass
-    def read_and_convert(filename, *args):
-        pass
+def read_csv_file(filename, delimeter=",", column_names=list()):
+    '''
+        This function reads the CSV file and converts it into pandas Dataframe
+    '''
+    csv_data = pd.read_csv(filepath_or_buffer=filename,
+                            sep=delimeter,
+                            usecols=column_names,
+                            skip_blank_lines=True,
+                            encoding="UTF-8",
+                            engine="python")
+    return csv_data
+
+
+def read_and_convert(filename, delimeter=",", column_names=list()):
+    print(column_names)
+    csv_data = read_csv_file(filename, delimeter, column_names)
+
+    return csv_data
+
 
 
 if __name__ == "__main__":
-    #read_and_convert("filename.csv")
-    pass
+    PROJECT_PATH = os.path.join(os.getcwd())
+    filename = PROJECT_PATH + "\csv" + "\\"+ "2022_Forbes_list.csv"
+    print(filename)
+    col_names = ["Rank in India", "Name","Headquarters","Revenue(billions US$)","Profit(billions US$)","Assets(billions US$)","Value(billions US$)","Industry"]
+    csv_data = read_and_convert(filename=filename, delimeter=",", column_names=col_names)
+
+    print(type(csv_data))
+
+
