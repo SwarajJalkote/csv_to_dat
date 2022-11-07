f9b1f8582b7e49a2c8c9c33f017fbc59db0b6b55
Swaraj Jalkote <swarajjalkote98@gmail.com>
2022-11-07 19:59:01 +0530

    Improved the functions

--- src/__init__.py
+++ src/__init__.py
@@ -2,5 +2,4 @@ import os
 import sys
 
 PROJECT_PATH = os.getcwd()
-SOURCE_PATH = os.path.join(PROJECT_PATH, "src")
-sys.path.append(SOURCE_PATH)
+sys.path.append(PROJECT_PATH)
--- src/csv_to_dat.py
+++ src/csv_to_dat.py
@@ -2,15 +2,18 @@
 import pandas as pd
 import typing
 import traceback
-import os
 
 
-def read_csv_file(filename, delimeter=",", column_names=list()):
+def read_csv_file(input_file, input_del=",", column_names=list()) -> pd.DataFrame:
     '''
-        This function reads the CSV file and converts it into pandas Dataframe
+        This function reads the CSV file and converts it into pandas Dataframe.\n
+        Description:
+            input_file: csv file which needs to be converted with complete path
+            input_del: delimiter used in input file. Default: , (comma)
+            column_names: list of names of columns which you want to extract from csv
     '''
-    csv_data = pd.read_csv(filepath_or_buffer=filename,
-                            sep=delimeter,
+    csv_data = pd.read_csv(filepath_or_buffer=input_file,
+                            sep=input_del,
                             usecols=column_names,
                             skip_blank_lines=True,
                             encoding="UTF-8",
@@ -18,35 +21,42 @@ def read_csv_file(filename, delimeter=",", column_names=list()):
     return csv_data
 
 
-def convert_to_dat(csv_data, output_path="output.dat"):
+def convert_to_dat(csv_data, output_path="output.dat") -> str:
     '''
-        This function will convert the Dataframe to dat
+        This function will convert the Dataframe to dat.\n
+         Description:
+            csv_data: pandas DataFrame returned by read_csv()
+            output_path: dat file which will be created as output with expected path
     '''
-    for i in range(3):
-        if output_path.endswith(".dat"):
-            try:
-                csv_data.to_csv(path_or_buf=output_path, sep="|", header=True, index=False, mode="w", encoding="UTF-8")
-                return f"File created successfully in {output_path}" 
-            except TypeError:
-                print("Please provide correct path with .dat file")
-                traceback.print_exc()
-        else:
-            output_path = input("Please enter correct output path with .dat. Example: output.dat >")
-
+    
+    if output_path.endswith(".dat"):
+        try:
+            csv_data.to_csv(path_or_buf=output_path, sep="|", header=True, index=False, mode="w", encoding="UTF-8")
+            return f"File created successfully in {output_path}" 
+        except Exception as exception:
+            print("Exception :", exception)
+            traceback.print_exc()
+    else:
+        raise Exception("Please provide correct path with .dat file")
+    
     return "Failed to provide correct output path"
 
 
-def read_and_convert(filename, delimeter=",", column_names=list()):
+def csv_to_dat(input_file, input_del=",", output_path="output.dat", output_del="|", column_names=list()) -> str:
     '''
-        This function will convert the csv to dat
+        This function will convert the csv to dat.\n
+        Description:
+            input_file: csv file which needs to be converted with complete path
+            input_del: delimiter used in input file. Default: , (comma)
+            output_path: dat file which will be created as output with expected path
+            output_del: delimiter used in output file: Default: | (pipe)
+            column_names: list of names of columns which you want to extract from csv
     '''
-    csv_data = read_csv_file(filename, delimeter, column_names)
-    response = convert_to_dat(csv_data=csv_data, output_path="csv/output.dat")
+    csv_data = read_csv_file(input_file, input_del, column_names)
+    response = convert_to_dat(csv_data=csv_data, output_path=f"csv/{output_path}")
     return response
 
-if __name__ == "__main__":
-    filename = "csv/2022_Forbes_list.csv"
-    col_names = ["Rank in India", "Name","Headquarters","Revenue(billions US$)","Profit(billions US$)","Assets(billions US$)","Value(billions US$)","Industry"]
-    response_message = read_and_convert(filename=filename, delimeter=",", column_names=col_names)
-    
-    print(response_message)
+# if __name__ == "__main__":
+#     filename = "csv/2022_Forbes_list.csv"
+#     col_names = ["Rank in India", "Name","Headquarters","Revenue(billions US$)","Profit(billions US$)","Assets(billions US$)","Value(billions US$)","Industry"]
+#     response_message = read_and_convert(filename=filename, delimeter=",", column_names=col_names)
